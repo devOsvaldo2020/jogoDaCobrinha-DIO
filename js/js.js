2a042a22199720ce57228a9b719397112f5d3399
osvaldo <71652778+devOsvaldo2020@users.noreply.github.com>
2022-03-04 14:10:35 -0300

    criando o jogo pelo dio

---
+++ js/js.js
@@ -0,0 +1,21 @@
+
+// **** js.js ****
+
+function start() { // Inicio da fun��o start()
+    
+	$("#inicio").hide();
+	
+	$("#fundoGame").append("<div id='jogador' class='anima1'></div>");
+	$("#fundoGame").append("<div id='inimigo1'></div>");
+	$("#fundoGame").append("<div id='inimigo2'></div>");
+	$("#fundoGame").append("<div id='amigo'></div>");
+    
+} // Fim da fun��o start
+
+
+
+    // **** index.html ****
+    
+    // <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+    // <script type="text/javascript" src="js/js.js"></script> 
+    
